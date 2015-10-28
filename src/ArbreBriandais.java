768b5a46aa89288009156117bb529d557bed3956
Marie Schindler <schindler.marie@gmail.com>
2015-10-28 12:53:04 +0100

    Ajout primitive addAB(String word)

--- src/ArbreBriandais.java
+++ src/ArbreBriandais.java
@@ -25,6 +25,18 @@ public class ArbreBriandais {
 		}
 	}
 	
+	public char getKey(){
+		return this.key; 
+	}
+	
+	public ArbreBriandais getFils(){
+		return this.fils; 
+	}
+	
+	public ArbreBriandais getNext(){
+		return this.next;
+	}
+	
 	public boolean isEmpty(){
 		if (this == null || (this.key=='\0' && this.fils==null && this.next==null)){
 			return true; 			
@@ -33,6 +45,41 @@ public class ArbreBriandais {
 		}
 	}
 	
+	public ArbreBriandais addAB(String word){
+		
+		if(this.isEmpty()){
+			if(word.equals("\0")){
+				return this; 
+			}
+			return new ArbreBriandais(word); 
+		}
+		
+		if(word.equals("\0")){
+			if(this.key == '\0'){
+				return this; 
+			}
+			return new ArbreBriandais('\0', null, this); 
+		}
+		
+		if(word.charAt(0)- this.key <0){
+
+			return new ArbreBriandais(word.charAt(0), new ArbreBriandais(word.substring(1)), this); 
+
+		}else if(word.charAt(0) - this.key > 0){
+			return new ArbreBriandais(this.key, this.fils, this.next.addAB(word)); 
+		}else{
+			if(this.fils.isEmpty()){
+				if(word.equals("\0")){
+					return this; 
+				}
+				return new ArbreBriandais(this.key, new ArbreBriandais('\0', null, new ArbreBriandais(word.substring(1))), this.next); 
+			}
+			return new ArbreBriandais(this.key, this.fils.addAB(word.substring(1)), this.next); 
+		}
+	}
+
+
+	}
 
 	
 	
