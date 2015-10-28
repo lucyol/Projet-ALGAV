7e7d620f032d9bda66fa135f8b43ac3899ad298d
Marie Schindler <schindler.marie@gmail.com>
2015-10-28 12:03:41 +0100

    Ajout constructeur et primitive isEmpty

--- src/ArbreBriandais.java
+++ src/ArbreBriandais.java
@@ -3,5 +3,40 @@ public class ArbreBriandais {
 	private char key; 
 	private ArbreBriandais fils; 
 	private ArbreBriandais next; 
+	
+	/*Primitives*/
+	public ArbreBriandais(){
+		key='\0'; 
+		fils=null; 
+		next=null; 		
+	}
+	
+	public ArbreBriandais(char key, ArbreBriandais fils, ArbreBriandais next){
+		this.key=key; 
+		this.fils=fils; 
+		this.next=next;
+	}
+	
+	public ArbreBriandais (String word){
+		if(word.equals("\0")){
+			 new ArbreBriandais(); 
+		}else{
+			new ArbreBriandais(word.charAt(0), new ArbreBriandais(word.substring(1)),null); 
+		}
+	}
+	
+	public boolean isEmpty(){
+		if (this == null || (this.key=='\0' && this.fils==null && this.next==null)){
+			return true; 			
+		}else{
+			return false; 
+		}
+	}
+	
+
+	
+	
+	
+	
 
 }
