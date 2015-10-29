b5892129662c1d6d9ea00a82320aa6241c65d5d5
Marie Schindler <schindler.marie@gmail.com>
2015-10-29 17:53:02 +0100

    Ajout methodes tail et head + correction des constructeurs

--- src/ArbreBriandais.java
+++ src/ArbreBriandais.java
@@ -3,87 +3,90 @@ public class ArbreBriandais {
 	private char key; 
 	private ArbreBriandais fils; 
 	private ArbreBriandais next; 
-	
+
 	/*Primitives*/
 	public ArbreBriandais(){
-		key='\0'; 
-		fils=null; 
-		next=null; 		
+		this('@', null, null);		
 	}
-	
+
 	public ArbreBriandais(char key, ArbreBriandais fils, ArbreBriandais next){
 		this.key=key; 
 		this.fils=fils; 
 		this.next=next;
 	}
-	
-	public ArbreBriandais (String word){
-		if(word.equals("\0")){
-			 new ArbreBriandais(); 
-		}else{
-			new ArbreBriandais(word.charAt(0), new ArbreBriandais(word.substring(1)),null); 
+/*On utilise une fonction statique pour cette primitive de construction 
+ * parce que java n'autorise pas l'utilisation recursive des constructeurs*/
+	public static ArbreBriandais arbreBriandais (String word){	
+		if(word.equals("")){
+			return new ArbreBriandais(); 
 		}
+		 return new ArbreBriandais(head(word), arbreBriandais(tail(word)),null); 
 	}
-	
+
 	public char getKey(){
 		return this.key; 
 	}
-	
+
 	public ArbreBriandais getFils(){
 		return this.fils; 
 	}
-	
+
 	public ArbreBriandais getNext(){
 		return this.next;
 	}
-	
+
 	public boolean isEmpty(){
-		if (this == null || (this.key=='\0' && this.fils==null && this.next==null)){
+		if (this.key=='@' && this.fils==null && this.next==null){
 			return true; 			
 		}else{
 			return false; 
 		}
 	}
-	
+
 	public ArbreBriandais addAB(String word){
-		
+
 		if(this.isEmpty()){
-			if(word.equals("\0")){
+			if(word.equals("")){
 				return this; 
 			}
-			return new ArbreBriandais(word); 
+			return  arbreBriandais(word); 
 		}
-		
-		if(word.equals("\0")){
-			if(this.key == '\0'){
+
+		if(word.equals("")){
+			if(this.key == '@'){
 				return this; 
 			}
-			return new ArbreBriandais('\0', null, this); 
+			return new ArbreBriandais('@', null, this); 
 		}
-		
+
 		if(word.charAt(0)- this.key <0){
 
-			return new ArbreBriandais(word.charAt(0), new ArbreBriandais(word.substring(1)), this); 
+			return new ArbreBriandais(head(word), arbreBriandais(tail(word)), this); 
 
-		}else if(word.charAt(0) - this.key > 0){
+		}else if(head(word) - this.key > 0){
 			return new ArbreBriandais(this.key, this.fils, this.next.addAB(word)); 
 		}else{
 			if(this.fils.isEmpty()){
-				if(word.equals("\0")){
+				if(word.equals("")){
 					return this; 
 				}
-				return new ArbreBriandais(this.key, new ArbreBriandais('\0', null, new ArbreBriandais(word.substring(1))), this.next); 
+				return new ArbreBriandais(this.key, new ArbreBriandais('@', null, arbreBriandais(tail(word))), this.next); 
 			}
-			return new ArbreBriandais(this.key, this.fils.addAB(word.substring(1)), this.next); 
+			return new ArbreBriandais(this.key, this.fils.addAB(tail(word)), this.next); 
 		}
 	}
-
-
+	
+	private static char head(String word){
+		return word.charAt(0); 
+	}
+	
+	private static String tail(String word){
+		return word.substring(1); 
 	}
 
-	
-	
-	
-	
 
 }
+
+
+
+
--- src/ArbreBriandaisTest.java
+++ src/ArbreBriandaisTest.java
@@ -8,12 +8,18 @@ public class ArbreBriandaisTest {
 	public static void main(String[] args) {
 
 		ArbreBriandais ab1= new ArbreBriandais(); 
-		ArbreBriandais ab2= new ArbreBriandais('\0', null, null); 
-		ArbreBriandais ab3= new ArbreBriandais("\0");
+		ArbreBriandais ab2= new ArbreBriandais('@', null, null); 
+		ArbreBriandais ab3=  ArbreBriandais.arbreBriandais("");
+		ArbreBriandais ab4 = new ArbreBriandais('B', null, null); 
+		ArbreBriandais ab5 = ArbreBriandais.arbreBriandais("Bonjour"); 
 		
 		System.out.println("ab1.isEmpty() = "+ ab1.isEmpty()); 		
 		System.out.println("ab2.isEmpty() = "+ ab2.isEmpty());
 		System.out.println("ab3.isEmpty() = "+ ab3.isEmpty()); 
+		System.out.println("ab4.isEmpty() = "+ ab4.isEmpty());
+		System.out.println("ab5.isEmpty() = "+ ab5.isEmpty());
+		
+		System.out.println(ab5.toString()); 
 	}
 
 }
